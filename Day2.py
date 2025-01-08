381a50d6775008daff86d3e07071ca8ebeae6189
Gundu Vinay <gunduvinay07@gmail.com>
2025-01-08 14:35:30 +0530

    Day to code solution

---
+++ Day2.py
@@ -0,0 +1,16 @@
+List=[1,2,6,8,10]
+print("\nList:",List)
+print("number at index 0:",List[0])
+print("number at index n:",List[3])
+tuple=('banana','apple','date','cat')
+print("\nTuple:",tuple)
+print("Element at 0 index:",tuple[0])
+print("Element at 1 index:",tuple[3])
+print("Element at 5 index:",tuple[2])
+
+dict={'name':'vinnu','sname':'gundu',
+      'roll_no':560,}
+print("\ndict:",dict)
+print("dict value and key at 0 index:",dict['name'])
+print("dict value and key at 0 index:",dict['sname'])
+print("dict value and key at 0 index:",dict['roll_no'])
